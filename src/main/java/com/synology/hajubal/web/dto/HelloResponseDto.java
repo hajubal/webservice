0dea5bf48c60f906eb5063451046f9f6d3e1be0a
hajubal <hajubal@gmail.com>
2021-02-12 18:56:34 +0900

    lombok 적용

--- src/main/java/com/synology/hajubal/web/dto/HelloResponseDto.java
+++ src/main/java/com/synology/hajubal/web/dto/HelloResponseDto.java
@@ -1,19 +1,11 @@
 package com.synology.hajubal.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
     private final String name;
     private final int amount;
-
-    public HelloResponseDto(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAmount() {
-        return this.amount;
-    }
 }
